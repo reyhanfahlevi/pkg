51c96093daa898ca1e2868cf546176a0fab18d96
reyhanfahlevi <reyhan.fahlevi@tokopedia.com>
2020-01-24 14:36:55 +0700

    feat: add randomizer for generate token

---
+++ go/token/token.go
@@ -0,0 +1,27 @@
+package rand
+
+import "crypto/rand"
+
+// GenerateString will generate random string token
+func GenerateString(n int) (string, error) {
+	const letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-"
+	bytes, err := GenerateBytes(n)
+	if err != nil {
+		return "", err
+	}
+	for i, b := range bytes {
+		bytes[i] = letters[b%byte(len(letters))]
+	}
+	return string(bytes), nil
+}
+
+// GenerateBytes will generate random []byte token
+func GenerateBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
---
+++ go/token/token_test.go
@@ -0,0 +1,61 @@
+package rand
+
+import (
+	"testing"
+)
+
+func TestGenerateBytes(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Test Success",
+			args: args{
+				n: 1,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := GenerateBytes(tt.args.n)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GenerateBytes() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestGenerateString(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Test Success",
+			args: args{
+				n: 10,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := GenerateString(tt.args.n)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GenerateString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
